cacfd4f34492a4c29a269c29da4bdf611b946dd2
FelixTe <73947483+FelixTe@users.noreply.github.com>
2023-10-05 10:52:57 +0200

    Update as clean as it gets untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -1,15 +1,6 @@
-
-
 import requests
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-
-import pandas as pd
-import matplotlib.pyplot as plt
-import time
-import random
 
 
 auth_url = "https://www.strava.com/oauth/token"
@@ -27,7 +18,6 @@ payload = {
 # Insert refresh tokens here
 # =============================================================================
 refresh_tokens = ['']
-
 
 payload['refresh_token']=refresh_tokens[0]
 print("Requesting Token...\n")
@@ -49,6 +39,3 @@ my_dataset = requests.get(activites_url, headers=header, params=param).json()
 id_last_tour = my_dataset[0]['id']
 segments_url = 'https://www.strava.com/api/v3/activities/'+str(id_last_tour)+'?include_all_efforts=True'
 my_segments = requests.get(segments_url, headers=header, params=param).json()
-
-
-
