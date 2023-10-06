63b767a2a214e768adfa188034be3c73554a72b3
Becker <F.Becker@dlr.de>
2023-10-06 10:48:37 +0200

    Add encode function to generate new encoded access tokens or change password

--- rettich_encrypt.py
+++ rettich_encrypt.py
@@ -2,6 +2,17 @@
 # https://stackoverflow.com/a/2490718/1675586
 import six, base64
 
+# use to generate new passwords or encode new access tokens
+def encode(key, string):
+    encoded_chars = []
+    for i in range(len(string)):
+        key_c = key[i % len(key)]
+        encoded_c = chr(ord(string[i]) + ord(key_c) % 256)
+        encoded_chars.append(encoded_c)
+    encoded_string = ''.join(encoded_chars)
+    encoded_string = encoded_string.encode('latin') if six.PY3 else encoded_string
+    return base64.urlsafe_b64encode(encoded_string).rstrip(b'=')
+
 def decode(key, string):
     string = base64.urlsafe_b64decode(string + b'===')
     string = string.decode('latin') if six.PY3 else string
