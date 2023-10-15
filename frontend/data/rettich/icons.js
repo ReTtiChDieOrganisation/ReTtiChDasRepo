b1862ee43c8c24c3c3e2e291e6e6218640b36f34
Florian Becker <florianbecker@Florians-MBP.fritz.box>
2023-10-15 22:53:33 +0200

    clean up

--- frontend/data/rettich/icons.js
+++ frontend/data/rettich/icons.js
@@ -3,35 +3,26 @@ var rettich_profile_pictures = {
     flo :
     {
         picture : "./frontend/icons/profile_pictures/flo.png",
-        size_x : 33, // 3:4 ratio
-	    size_y : 44, // 3:4 ratio
-        anchor_x : 33/2, // picture_size_x/2
-	    anchor_y : 44+22 // picture_size_y + adjust (adjust to center picture in frame)
+        size : [33,44], // 3:4 ratio
+        anchor : [33/2,44+22] // picture_size_y + adjust (adjust to center picture in frame)
     },
-    // TODO: add pictures.
     felix :
     {
         picture : "./frontend/icons/profile_pictures/felix.png",
-        size_x : 33, // 3:4 ratio
-	    size_y : 44, // 3:4 ratio
-        anchor_x : 33/2, // picture_size_x/2
-	    anchor_y : 44+22 // picture_size_y + adjust (adjust to center picture in frame)
+        size : [33,44], // 3:4 ratio
+        anchor : [33/2,44+22] // picture_size_y + adjust (adjust to center picture in frame)
     },
     philipp :
     {
         picture : "./frontend/icons/profile_pictures/philipp.png",
-        size_x : 33, // 3:4 ratio
-	    size_y : 44, // 3:4 ratio
-        anchor_x : 33/2, // picture_size_x/2
-	    anchor_y : 44+22 // picture_size_y + adjust (adjust to center picture in frame)
+        size : [33,44], // 3:4 ratio
+        anchor : [33/2,44+22] // picture_size_y + adjust (adjust to center picture in frame)
     },
     default :
     {
         picture : "./frontend/icons/profile_pictures/default.png",
-        size_x : 33, // 3:4 ratio
-	    size_y : 44, // 3:4 ratio
-        anchor_x : 33/2, // picture_size_x/2
-	    anchor_y : 44+22 // picture_size_y + adjust (adjust to center picture in frame)
+        size : [33,44], // 3:4 ratio
+        anchor : [33/2,44+22] // picture_size_y + adjust (adjust to center picture in frame)
     }
 };
 
@@ -41,110 +32,88 @@ var rettich_frames = {
         frame_fg : "./frontend/icons/frames/fg_blue.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+0+" ,"+56+","+123+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     green :
     {
         frame_fg : "./frontend/icons/frames/fg_green.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+6+" ,"+80+","+0+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     navyblue :
     {
         frame_fg : "./frontend/icons/frames/fg_navyblue.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+32+" ,"+56+","+100+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     neutral :
     {
         frame_fg : "./frontend/icons/frames/fg_neutral.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+208+" ,"+206+","+206+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     pride_fg :
     {
         frame_fg : "./frontend/icons/frames/fg_pride.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+208+" ,"+206+","+206+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     pride_bg :
     {
         frame_fg : "./frontend/icons/frames/fg_neutral.png",
         frame_bg : "./frontend/icons/frames/bg_pride.png",
         line_color : "rgba("+208+" ,"+206+","+206+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     purple :
     {
         frame_fg : "./frontend/icons/frames/fg_purple.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+112+" ,"+48+","+160+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     red :
     {
         frame_fg : "./frontend/icons/frames/fg_red.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+139+" ,"+0+","+0+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     ukraine :
     {
         frame_fg : "./frontend/icons/frames/fg_ukraine.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+255+" ,"+238+","+21+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     yellow :
     {
         frame_fg : "./frontend/icons/frames/fg_yellow.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+255+" ,"+238+","+21+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     default :
     {
         frame_fg : "./frontend/icons/frames/fg_neutral.png",
         frame_bg : "./frontend/icons/frames/bg_white.png",
         line_color : "rgba("+0+" ,"+0+","+0+")",
-        size_x : 54, // 3:4 ratio
-        size_y : 72, // 3:4 ratio
-        anchor_x : 54/2, // frame_size_x/2
-        anchor_y : 72 // frame_size_y
+        size : [54,72], // 3:4 ratio
+        anchor : [54/2,72] // frame_size_x/2
     },
     /* TODO: 
      * black
@@ -153,4 +122,19 @@ var rettich_frames = {
      * silver
      * gold
      */
+};
+
+var rettich_other_icons = {
+    start :
+    {
+        icon : "./frontend/icons/other/start.png",
+        size : [15,15],
+        anchor : [8,8]
+    },
+    finish :
+    {
+        icon : "./frontend/icons/other/finish.png",
+        size : [15,15],
+        anchor : [8,8]
+    },
 };
